1d73220aa10757290375c4cd8524daf6b72d6652
happyxurde1003 <success.xurde.boss@gmail.com>
2022-03-08 06:18:39 -0800

    add attention on key

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
+  ///Pay attention: it is secret key!
   mongoURI: "mongodb+srv://alvindavis191:welcome!0428*@cluster0.xvwuu.mongodb.net/recipe",
   jwtSecret: "PbYeXaFUU5iZnucA2bTVUL+BIfPR7D0Pg4k8HGJS4jzoSbOHzf+Nh5ExPwmC8/VqfpkKiXGabzWRjqs7wY3nGlOPJVREoKFnMtmj1ecdvDUYKkCqm4p2PI3lqfa5wMmhhu/6AzNvFTzWsVn/XVoj87ps6er42v47CZB2Qzs99S3f9lnE+dxVmLq21sdX+MOG/cNxiej25cew0l1nZDVukmiCyZ0oSH2M2Rqx7j06vmARigXxh+Fk1vOLNQ0Sc5XKvPYoqLvkVENmpZPiLMGQTEvV6d/UO+evroBcw0J7Dqq3SVFwB9rnZVGlAhGPQAGf3MNZRtERpDsfjPxVvFgzww=="
 };
