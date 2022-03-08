94c800507efc339040672434bce5cd70e7e35f5a
happyxurde1003 <success.xurde.boss@gmail.com>
2022-03-08 05:45:41 -0800

    udpate git files

---
+++ client/build/precache-manifest.f025eb4a0ec0a7f980576c4599e868e8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "bebaef6e5f49efd76b60",
+    "url": "/static/css/main.331dd350.chunk.css"
+  },
+  {
+    "revision": "bebaef6e5f49efd76b60",
+    "url": "/static/js/main.8bef69eb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "66228d226f238d526256",
+    "url": "/static/js/2.82002269.chunk.js"
+  },
+  {
+    "revision": "eaacb92ff24ba746aba371412618413c",
+    "url": "/static/media/logo-dark.eaacb92f.svg"
+  },
+  {
+    "revision": "da8615d5c6bc6e5f301edefdfb32dff4",
+    "url": "/static/media/Grunge-Paper-Texture-1.da8615d5.jpg"
+  },
+  {
+    "revision": "c1220b3c54a1f23376445467a54764ac",
+    "url": "/static/media/ornament2.c1220b3c.svg"
+  },
+  {
+    "revision": "f849a7b75ffb0ec3ef402617af464e8c",
+    "url": "/static/media/hr-line.f849a7b7.svg"
+  },
+  {
+    "revision": "2123952abc7197dc31e0e68a65cba07b",
+    "url": "/static/media/wood.2123952a.jpeg"
+  },
+  {
+    "revision": "adf07c9bc8da10ea6f0ffb767f65cc1a",
+    "url": "/static/media/home-bg.adf07c9b.jpg"
+  },
+  {
+    "revision": "eaffa18d0e230df1b9ea7c4d852930b3",
+    "url": "/index.html"
+  }
+];
