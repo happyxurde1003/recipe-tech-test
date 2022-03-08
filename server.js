6cc9eff12f56479f31f712bd7f8d99881983ec75
happyxurde1003 <success.xurde.boss@gmail.com>
2022-03-08 05:59:21 -0800

    fixed path error

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const config = require("./config/keys.js");
+const path = require('path')
 
 const app = express();
 
